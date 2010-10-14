fa892ceb2daf15904f251390d255a15752e9f260
Arthur Kalmenson <arthur.kalm@gmail.com>
2010-10-14 17:24:22 -0400

    First attempt at the new data binding Editor stuff.

--- src/main/java/org/devdays/client/CustomerDemo.java
+++ src/main/java/org/devdays/client/CustomerDemo.java
@@ -1,18 +1,42 @@
 package org.devdays.client;
 
+import org.devdays.client.binding.CustomerEditor;
+import org.devdays.shared.Customer;
+
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.editor.client.SimpleBeanEditorDriver;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.RootPanel;
 
-/**
- * Entry point classes define <code>onModuleLoad()</code>.
- */
 public class CustomerDemo implements EntryPoint {
 
-	/**
-	 * This is the entry point method.
-	 */
+	interface Driver extends SimpleBeanEditorDriver<Customer, CustomerEditor> {
+	}
+
+	Driver driver = GWT.create(Driver.class);
+
 	public void onModuleLoad() {
-		RootPanel.get().add(new Label("Hello, World!"));
+		CustomerEditor editor = new CustomerEditor();
+		driver.initialize(editor);
+		driver.edit(new Customer("1", "Bob", "Smith"));
+
+		RootPanel.get().add(editor);
+
+		Button button = new Button("Get Data");
+		RootPanel.get().add(button);
+
+		button.addClickHandler(new ClickHandler() {
+
+			public void onClick(ClickEvent arg0) {
+				Customer customer = driver.flush();
+				Window.alert("id: " + customer.getId() + ", first name: "
+						+ customer.getFirstName() + ", last name: "
+						+ customer.getLastName());
+			}
+		});
 	}
 }
---
+++ src/main/java/org/devdays/client/binding/CustomerEditor.java
@@ -0,0 +1,30 @@
+package org.devdays.client.binding;
+
+import org.devdays.shared.Customer;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.editor.client.Editor;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.Widget;
+
+public class CustomerEditor extends Composite implements Editor<Customer> {
+
+	private static CustomerEditorUiBinder uiBinder = GWT
+			.create(CustomerEditorUiBinder.class);
+
+	interface CustomerEditorUiBinder extends UiBinder<Widget, CustomerEditor> {
+	}
+
+	@UiField
+	TextBox firstName;
+	
+	@UiField
+	TextBox lastName;
+
+	public CustomerEditor() {
+		initWidget(uiBinder.createAndBindUi(this));
+	}
+}
